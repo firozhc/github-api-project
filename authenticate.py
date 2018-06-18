ff23e9bc100731cb53c4e88b36e7801514856127
Firoz <emperorscourge@gmail.com>
2018-06-18 14:23:13 +0530

    fixed auth

--- authenticate.py
+++ authenticate.py
@@ -2,19 +2,30 @@ import requests
 import json
 
 
-def authenticate(url):
-	username = "emperorscourge@gmail.com"
-	password = "Apple_123"
+def auth(url, username, password):
+	
+	headers = { "Accept": "application/vnd.github.v3+json",
+				"Content-Type": "application/json; charset=utf-8"
+			  }
 
-	headers = { "Accept": "application/vnd.github.v3+json", "Content-Type": "application/json; charset=utf-8"}
+	r = requests.request("GET", url, headers=headers, auth=(username, password))
+	
+	rjson = json.loads(r.text)
 
-	r = requests.request("GET", url, headers=headers)
-	print(r.text)
+	print(r.headers)
+
+	for key, value in rjson.items():
+		print(str(key)+": "+ str(value))
+	
 	
 
 def main():
-	base_url = "https://api.github.com"
-	authenticate(base_url)
+
+	base_url = "https://api.github.com/users/firozhc"
+	username = "emperorscourge@gmail.com"
+	password = "Apple_123"
+
+	auth(base_url, username, password)
 
 if __name__=="__main__":
 	main()
