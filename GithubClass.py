b3b93e95a86c1f84e25790914659267520b04998
Firoz Hossain Chaudhuri <emperorscourge@gmail.com>
2018-06-19 09:14:14 -0400

    renamed class file

---
+++ GithubClass.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+class Github:
+
+	base_url = ""
+	__init__(self, username, password):
+	self.username = username
+	self.password = password
+
+
+
+	def auth_module(self, )
+
